74e14a181bbff63a1047051a727ef2c9c9c3613e
Arthur <arthur@alternumerica.org>
2022-03-09 17:58:41 +0100

    petites améliorations ergonomiques

--- resources/views/livewire/arrivals-and-departures-today.blade.php
+++ resources/views/livewire/arrivals-and-departures-today.blade.php
@@ -1,14 +1,14 @@
-<div class="text-center">
+<div class="text-center mt-4">
     @if ( $arrivals->count() )
         <button class="mx-4" wire:click="$emit('displayToday', 'arrivals')">{{ $arrivals->getTotalAmountOfVisitors() }} {{__("arrivées aujourd'hui")}}</button>
     @else
-        <span class="mx-4">{{__("Pas d'arrivée aujourd'hui")}}</span>
+        <span class="mx-4"><nobr>{{__("Pas d'arrivée aujourd'hui")}}</nobr></span>
     @endif
 
     @if ( $departures->count() )
         <button class="mx-4" wire:click="$emit('displayToday', 'departures')">{{ $departures->getTotalAmountOfVisitors() }} {{__("départs aujourd'hui")}}</button>
     @else
-        <span class="mx-4">{{ __("Pas de départ aujourd'hui") }}</span>
+        <span class="mx-4"><nobr>{{ __("Pas de départ aujourd'hui") }}</nobr></span>
     @endif
 
 </div>
